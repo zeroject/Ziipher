295533629a126fc70f072728006e7a79b87cfeed
Casper Fink <47432671+zeroject@users.noreply.github.com>
2024-02-22 17:50:53 +0100

    fixed issues
    some projects where hidden

---
+++ CommentService/Controllers/CommentController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommentService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CommentController : ControllerBase
+    {
+        public CommentController()
+        {
+        }
+
+        /// <summary>
+        /// Returns all comments for a post with the given @PostID and paginates the results.
+        /// </summary>
+        /// <param name="pageSize"></param>
+        /// <param name="pageNumber"></param>
+        /// <param name="PostID"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetComments")]
+        public IActionResult GetComments(int pageSize, int pageNumber, int PostID)
+        {
+            return Ok("GetComments");
+        }
+
+        /// <summary>
+        /// Adds a comment to the post with the @PostID.
+        /// </summary>
+        /// <param name="PostID"></param>
+        /// <param name="Comment"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("AddComment")]
+        public IActionResult AddComment(int PostID, string Comment)
+        {
+            return Ok("AddComment");
+        }
+
+
+        /// <summary>
+        /// Updates the comment with the given @CommentID.
+        /// </summary>
+        /// <param name="CommentID"></param>
+        /// <param name="Comment"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("UpdateComment")]
+        public IActionResult UpdateComment(int CommentID, string Comment)
+        {
+            return Ok("UpdateComment");
+        }
+
+        /// <summary>
+        /// Deletes the comment with the given @CommentID.
+        /// </summary>
+        /// <param name="CommentID"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("DeleteComment")]
+        public IActionResult DeleteComment(int CommentID)
+        {
+            return Ok("DeleteComment");
+        }
+    }
+}
