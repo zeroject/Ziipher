970fbf61e470d04f1189362b2a665f6661f477f4
kasp441 <91524039+kasp441@users.noreply.github.com>
2024-02-22 18:24:02 +0100

    added base authController

---
+++ AuthenticationService/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using AuthenticationService.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.Controllers
+{
+    /// <summary>
+    /// AuthController for authentication and authorization
+    /// </summary>
+
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        /// <summary>
+        /// Constructor for AuthController
+        /// </summary>
+        public AuthController()
+        {
+
+        }
+
+        /// <summary>
+        /// takes in user credentials and issues a token for later validation
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("loginUser")]
+        public IActionResult LoginUser(LoginDto loginDto)
+        {
+            return Ok("User logged in successfully");
+        }
+
+        /// <summary>
+        /// removes the token from the active tokens pool
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("logoutUser")]
+        public IActionResult LogoutUser(string token)
+        {
+            return Ok("User logged out successfully");
+        }
+
+        /// <summary>
+        /// method to be called to validate the user's token
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("validateUser")]
+        public IActionResult validateUser(string token)
+        {
+            return token != null ? Ok("User is valid") : Ok("User is not valid");
+        }
+
+    }
+}
---
+++ AuthenticationService/Dto/LoginDto.cs
@@ -0,0 +1,18 @@
+﻿namespace AuthenticationService.Dto
+{
+    /// <summary>
+    /// object to hold user credentials
+    /// </summary>
+    public class LoginDto
+    {
+        /// <summary>
+        /// for validating. login. and other purposes
+        /// </summary>
+        public required string Username { get; set; }
+
+        /// <summary>
+        /// for validating. login. and other purposes
+        /// </summary>
+        public required string Password { get; set; }
+    }
+}
