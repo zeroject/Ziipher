2bce996cfee7cdbcedc596072a44fc77661b9b15
kasp441 <91524039+kasp441@users.noreply.github.com>
2024-02-23 17:52:25 +0100

    further update on base db functions. WIP

--- AuthenticationService/BE/LoginBe.cs
+++ AuthenticationService/BE/LoginBe.cs
@@ -1,6 +1,23 @@
 ﻿namespace AuthenticationService.BE
 {
+    /// <summary>
+    /// for validating. login. and EF purposes
+    /// </summary>
     public class LoginBe
     {
+        /// <summary>
+        /// primary key for the user table
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// identifier for the user
+        /// </summary>
+        required public string Username { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        required public string Password { get; set; }
     }
 }
