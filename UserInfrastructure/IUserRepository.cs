277849a5da188bc05471d5720410376d00140aba
Casper Fink <47432671+zeroject@users.noreply.github.com>
2024-02-24 22:27:52 +0100

    nealy done with crud
    wierd bug where it sometimes do not want to create a user

---
+++ UserInfrastructure/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInfrastructure
+{
+    public interface IUserRepository
+    {
+        public void AddUser(User user);
+        public void DeleteUser(int userID);
+        public void UpdateUser(User user);
+        public User GetUser(int userID);
+    }
+}
---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,107 @@
+﻿using Domain;
+using Domain.DTO_s;
+using Microsoft.AspNetCore.Mvc;
+using UserApplication;
+
+namespace UserService.Controllers
+{
+    /// <summary>
+    /// Controller for user management
+    /// </summary>
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserCrud _userCrud;
+        private readonly ILogger<UserController> _logger;
+        public UserController(IUserCrud userCrud, ILogger<UserController> logger)
+        {
+            _userCrud = userCrud;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Adds a user to the system
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("AddUser")]
+        public IActionResult AddUser(UserDTO user)
+        {
+            try
+            {
+                _userCrud.AddUser(user);
+                _logger.LogInformation("User added to the system");
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An internal error has occurred");
+                return StatusCode(500, "An internal Error has occurred");
+            }
+        }
+
+        /// <summary>
+        /// Deletes a user from the system
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("DeleteUser")]
+        public IActionResult DeleteUser(int userID)
+        {
+            try
+            {
+                _userCrud.DeleteUser(userID);
+                _logger.LogInformation("User deleted from the system");
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An internal error has occurred");
+                return StatusCode(500, "An internal Error has occurred");
+            }
+        }
+
+        /// <summary>
+        /// Returns a user from the system
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetUser")]
+        public IActionResult GetUser(int userID)
+        {
+            try
+            {
+                _logger.LogInformation("Getting user with ID: " + userID);
+                return Ok(_userCrud.GetUser(userID));
+            } catch (Exception e)
+            {
+                _logger.LogError(e, "An internal error has occurred");
+                return StatusCode(500, "An internal Error has occurred");
+            }
+        }
+
+        /// <summary>
+        /// Updates a user in the system
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("UpdateUser")]
+        public IActionResult UpdateUser(User user)
+        {
+            try
+            {
+                _userCrud.UpdateUser(user);
+                _logger.LogInformation("User updated in the system");
+                return Ok();
+            } catch (Exception e)
+            {
+                _logger.LogError(e, "An internal error has occurred");
+                return StatusCode(500, "An internal Error has occurred");
+            }
+        }
+    }
+}
