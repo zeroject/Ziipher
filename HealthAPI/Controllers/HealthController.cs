5ccbc6e5ba757cb8bfd74a7b55aab5e572ebc5b1
Casper Fink <47432671+zeroject@users.noreply.github.com>
2024-03-01 21:05:43 +0100

    creating the health service

---
+++ HealthAPI/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthAPI.Controllers
+{
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/health")]
+        public IActionResult Get(string service)
+        {
+            _logger.LogInformation("Request for health check resources for " + service);
+            return Ok("Healthy");
+        }
+    }
+}
